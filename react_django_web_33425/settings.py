a8cf8aa0d2e5745ec2cd0c6bd4ba7d7d5db78a99
itech@cyberservices.com <itech@cyberservices.com>
2022-02-17 01:03:33 +0100

    Update settings.py

--- react_django_web_33425/settings.py
+++ react_django_web_33425/settings.py
@@ -177,18 +177,30 @@ AWS_ACCESS_KEY_ID = env.str("AWS_ACCESS_KEY_ID", "")
 AWS_SECRET_ACCESS_KEY = env.str("AWS_SECRET_ACCESS_KEY", "")
 AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME", "")
 AWS_STORAGE_REGION = env.str("AWS_STORAGE_REGION", "")
+
+AWS_S3_CUSTOM_DOMAIN = env.str("AWS_S3_CUSTOM_DOMAIN", "")
+AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+AWS_DEFAULT_ACL = env.str("AWS_DEFAULT_ACL", "public-read")
+AWS_MEDIA_LOCATION = env.str("AWS_MEDIA_LOCATION", "media")
+AWS_AUTO_CREATE_BUCKET = env.bool("AWS_AUTO_CREATE_BUCKET", True)
+DEFAULT_FILE_STORAGE = env.str(
+    "DEFAULT_FILE_STORAGE", "react_django_web_33425.storage_backends.MediaStorage"
+)
+MEDIA_URL = '/mediafiles/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 AWS_DEFAULT_ACL = None
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
 # s3 static settings
-STATIC_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-STATICFILES_STORAGE = 'react_django_web_33425.storage_backends.StaticStorage'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, STATIC_LOCATION),)
-# s3 public media settings
-PUBLIC_MEDIA_LOCATION = 'media'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-DEFAULT_FILE_STORAGE = 'react_django_web_33425.storage_backends.PublicMediaStorage'
+# STATIC_LOCATION = 'static'
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+# STATICFILES_STORAGE = 'react_django_web_33425.storage_backends.StaticStorage'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, STATIC_LOCATION),)
+# # s3 public media settings
+# PUBLIC_MEDIA_LOCATION = 'media'
+# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+# DEFAULT_FILE_STORAGE = 'react_django_web_33425.storage_backends.PublicMediaStorage'
 
 LOGGING = {
     'version': 1,
