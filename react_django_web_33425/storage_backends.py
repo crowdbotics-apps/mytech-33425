704e958d5165b21c9696fa6a8a9cf0a9a8cb1e6e
itech@cyberservices.com <itech@cyberservices.com>
2022-02-17 01:06:25 +0100

    Update storage_backends.py

--- react_django_web_33425/storage_backends.py
+++ react_django_web_33425/storage_backends.py
@@ -1,12 +1,7 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
-class StaticStorage(S3Boto3Storage):
-    location = 'static'
-    default_acl = 'public-read'
 
-
-class PublicMediaStorage(S3Boto3Storage):
-    location = 'media'
-    default_acl = 'public-read'
+class MediaStorage(S3Boto3Storage):
+    location = settings.AWS_MEDIA_LOCATION
     file_overwrite = False
