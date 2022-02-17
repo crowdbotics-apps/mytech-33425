6ceb2356c158ec69ac43acfbd35053e93a012e16
itech@cyberservices.com <itech@cyberservices.com>
2022-02-18 00:49:13 +0100

    possible critical bug

--- react_django_web_33425/urls.py
+++ react_django_web_33425/urls.py
@@ -15,5 +15,3 @@ urlpatterns = [
     path('newsletter/', include("newsletter.urls")),
     path('api/', include("api.urls")),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
