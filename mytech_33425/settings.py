01c1dd5edf14bbf8e151affb62f2e6fbff9ab74c
itech@cyberservices.com <itech@cyberservices.com>
2022-02-22 23:35:20 +0100

    aws sig ver

--- mytech_33425/settings.py
+++ mytech_33425/settings.py
@@ -165,13 +165,12 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 # AWS S3 config
-from botocore.client import Config
 
 AWS_ACCESS_KEY_ID = env.str("AWS_ACCESS_KEY_ID", "")
 AWS_SECRET_ACCESS_KEY = env.str("AWS_SECRET_ACCESS_KEY", "")
 AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME", "")
 AWS_STORAGE_REGION = env.str("AWS_STORAGE_REGION", "")
-config=Config(signature_version='s3v4')
+AWS_S3_SIGNATURE_VERSION = "s3v4"
 
 USE_S3 = (
     AWS_ACCESS_KEY_ID and
